3a62400c3aa9033ac0efac3041b1d5e84fddde67
dormirMais <soonhak1212@naver.com>
2023-01-30 23:04:30 +0900

    game solution

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,43 @@
 /**
  * Let's make a game 🕹
  */
+
+const position = {
+  x: 0,
+  y: 0,
+};
+
+function move(direction: Direction): void {
+  switch (direction) {
+    case "up":
+      position.y += 1;
+      break;
+    case "down":
+      position.y -= 1;
+      break;
+    case "left":
+      position.x -= 1;
+      break;
+    case "right":
+      position.x += 1;
+      break;
+    default:
+      throw Error("unkown direction");
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
+
+type Direction = "up" | "down" | "left" | "right";
+type Position = {
+  x: number;
+  y: number;
+};
