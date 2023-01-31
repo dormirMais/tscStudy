056632c497f04aa9c3bc3db157c66218f8a9508b
dormirMais <soonhak1212@naver.com>
2023-01-31 13:49:17 +0900

    기초적인 인터페이스 적용

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -4,7 +4,11 @@
     hasMilk: boolean;
   };
 
-  class CoffeeMaker {
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
     private static BEANS_GRAMS_PER_SHOT: number = 7; // private키워드를 이용해서 외부에서 참조가 불가능하도록 만들어준다.
     private coffeeBeans: number = 0; // 반면 이렇게 일반적으로 정의하는 경우 Instance level로 정의된다.
 
@@ -12,8 +16,8 @@
       this.coffeeBeans = coffeBeans;
     }
 
-    static makeMachine(coffeeBeans: number): CoffeeMaker {
-      return new CoffeeMaker(coffeeBeans);
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
     }
 
     fillCoffeeBeans(beans: number) {
@@ -35,11 +39,11 @@
     }
 
     private grindBeans(shots: number) {
-      if (this.coffeeBeans < shots * CoffeeMaker.BEANS_GRAMS_PER_SHOT) {
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMS_PER_SHOT) {
         throw new Error("not enough coffee beans");
       }
       console.log(`grinding beans for ${shots}`);
-      this.coffeeBeans -= shots * CoffeeMaker.BEANS_GRAMS_PER_SHOT;
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMS_PER_SHOT;
       console.log(`${this.coffeeBeans} of coffeebeans left `);
     }
 
@@ -50,6 +54,11 @@
     }
   }
 
-  const maker = CoffeeMaker.makeMachine(32);
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  maker.fillCoffeeBeans(10);
   console.log(maker.makeCoffee(2));
+
+  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
+  maker2.fillCoffeeBeans(10);
+  console.log(maker2.makeCoffee(2));
 }
