d8bdda125c740b5a24a541b966f961a4a8ff44e1
dormirMais <soonhak1212@naver.com>
2023-01-31 14:58:10 +0900

    추상화

--- 3-oop/3-4-abstraction.ts
+++ 3-oop/3-4-abstraction.ts
@@ -7,8 +7,14 @@
   interface CoffeeMaker {
     makeCoffee(shots: number): CoffeeCup;
   }
+  interface CommercialCoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+    fillCoffeeBeans(benas: number): void;
+    clean(): void;
+  }
 
-  class CoffeeMachine implements CoffeeMaker {
+  class CoffeeMachine implements CoffeeMaker, CommercialCoffeeMaker {
+    // 이런식으로 interface는 multy implements가 가능하다.
     private static BEANS_GRAMS_PER_SHOT: number = 7; // private키워드를 이용해서 외부에서 참조가 불가능하도록 만들어준다.
     private coffeeBeans: number = 0; // 반면 이렇게 일반적으로 정의하는 경우 Instance level로 정의된다.
 
@@ -38,13 +44,18 @@
       };
     }
 
+    clean(): void {
+      console.log("cleaning the machine");
+    }
+
     private grindBeans(shots: number) {
       if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMS_PER_SHOT) {
         throw new Error("not enough coffee beans");
       }
+      console.log("현재 남은 커피의 양 => ", this.coffeeBeans);
       console.log(`grinding beans for ${shots}`);
       this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMS_PER_SHOT;
-      console.log(`${this.coffeeBeans} of coffeebeans left `);
+      console.log(`커피를 다 만들도 남은 커피의 양 => ${this.coffeeBeans} `);
     }
 
     makeCoffee(shots: number): CoffeeCup {
@@ -54,11 +65,40 @@
     }
   }
 
-  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
-  maker.fillCoffeeBeans(10);
-  console.log(maker.makeCoffee(2));
+  // const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  // maker.fillCoffeeBeans(10);
+  // console.log(maker.makeCoffee(2));
 
-  const maker2: CoffeeMaker = CoffeeMachine.makeMachine(32);
-  maker2.fillCoffeeBeans(10);
-  console.log(maker2.makeCoffee(2));
+  // const maker2: CommercialCoffeeMaker = CoffeeMachine.makeMachine(32);
+  // maker2.fillCoffeeBeans(32);
+  // console.log(maker2.makeCoffee(2));
+  // maker2.clean();
+
+  //================================================
+
+  class AmateurUser {
+    constructor(private machine: CoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+    }
+  }
+
+  class ProBarista {
+    constructor(private machine: CommercialCoffeeMaker) {}
+    makeCoffee() {
+      const coffee = this.machine.makeCoffee(2);
+      console.log(coffee);
+      // this.machine.fillCoffeeBeans(10);
+      // this.machine.clean();
+    }
+  }
+
+  const maker: CoffeeMachine = CoffeeMachine.makeMachine(32);
+  const amateur = new AmateurUser(maker);
+  const pro = new ProBarista(maker);
+  pro.makeCoffee();
+  setTimeout(() => {
+    amateur.makeCoffee();
+  }, 2000);
 }
