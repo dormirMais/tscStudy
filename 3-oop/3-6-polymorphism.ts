6be4ded41cd6ff38e89248634ed671e9baeb5c5c
dormirMais <soonhak1212@naver.com>
2023-01-31 17:04:45 +0900

    다형성 구현 완료

--- 3-oop/3-6-polymorphism.ts
+++ 3-oop/3-6-polymorphism.ts
@@ -1,7 +1,8 @@
 {
   type CoffeeCup = {
     shots: number;
-    hasMilk: boolean;
+    hasMilk?: boolean; // "?"를 이용해서 optional한 값을 만들 수 있다.
+    hasSugar?: boolean;
   };
 
   interface CoffeeMaker {
@@ -45,7 +46,7 @@
       console.log("현재 남은 커피의 양 => ", this.coffeeBeans);
       console.log(`grinding beans for ${shots}`);
       this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMS_PER_SHOT;
-      console.log(`커피를 다 만들고 남은 커피의 양 => ${this.coffeeBeans} `);
+      console.log(`커피를 다 만들고 남은 커피의 양 => `, this.coffeeBeans);
     }
 
     makeCoffee(shots: number): CoffeeCup {
@@ -72,10 +73,38 @@
     }
   } // 기본적으로 extends를 사용해서 상속이 가능하다.
 
-  const latteMachine = new CaffeLatteMachine(23, "aksjhdkajshd");
-  console.log(latteMachine.serialNumber);
-  const coffee = latteMachine.makeCoffee(1); //기본적으로 CaffeLatteMachine은 CoffeeMachine으로부터 상속을 받은 클래스이기 때문에 기존에 CoffeeMachine
-  //이 가지고 있는 메소드나 프로퍼티를 사용할 수 있다.
-  // +overwriting을 이용해서 해당 메소드를 덮어쓸 수 있다.
-  console.log(coffee);
+  class SweetCoffeMaker extends CoffeeMachine {
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return {
+        ...coffee,
+        hasSugar: true,
+      };
+    }
+  }
+
+  const machines: CoffeeMaker[] = [
+    //이렇게 implement된 interface를 사용하면 해당 interface에 있는 기능만 사용이 가능하다.
+    new CoffeeMachine(16),
+    new CaffeLatteMachine(16, "asd"),
+    new SweetCoffeMaker(16),
+    new CoffeeMachine(16),
+    new CaffeLatteMachine(16, "asd"),
+    new SweetCoffeMaker(16),
+  ];
+
+  /*
+  const machines: = [       이렇게 일반 적인 형태로 사용하면 요 아이들이 가지고 있는 공통 요소들을 사용할 수 있다.
+    new CoffeeMachine(16),
+    new CaffeLatteMachine(16, "asd"),
+    new SweetCoffeMaker(16),
+    new CoffeeMachine(16),
+    new CaffeLatteMachine(16, "asd"),
+    new SweetCoffeMaker(16),
+  ];
+  */
+  machines.forEach((machine) => {
+    console.log("==============================================");
+    machine.makeCoffee(1); //이게 바로 엄청난 장점이다. 어떤 클래스이던지 상관없이 하나의 메소드로 모두 동작을 수행하고 있다.
+  });
 }
