c1080bb39b529bc3be0adeec5eafe301e8e127a3
dormirMais <soonhak1212@naver.com>
2023-01-31 23:11:37 +0900

    coposition을 이용한 클래스 생성완료 but 각 모듈간에 간섭이 큼 => 해결필요

--- 3-oop/3-7-composition.ts
+++ 3-oop/3-7-composition.ts
@@ -1,4 +1,7 @@
 {
+  // 상속에는 근본적인 문제가 있다. 상속의 깊이가 깊어질 수록 상속의 관계가 복잡해진다. 또한 상속과정에서 복수개의 부모요소를 갖을 수 없다.
+  // 이를 해결하기 위해서 composition을 이용할 수 있다.
+  // 컴포지션은 필요한 것을 가져와서 조립해 나가는 것을 의미한다.
   type CoffeeCup = {
     shots: number;
     hasMilk?: boolean; // "?"를 이용해서 optional한 값을 만들 수 있다.
@@ -55,56 +58,79 @@
       return this.extract(shots);
     }
   }
-
-  class CaffeLatteMachine extends CoffeeMachine {
-    constructor(beans: number, public readonly serialNumber: string) {
-      super(beans);
-    }
-
+  // 싸구려 우유 거품기..
+  class CheapMilkSteamer {
     private steamMilk() {
       console.log("steaming some milk");
     }
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(shots);
-      return {
-        ...coffee,
-        hasMilk: true,
-      };
-    }
-  } // 기본적으로 extends를 사용해서 상속이 가능하다.
 
-  class SweetCoffeMaker extends CoffeeMachine {
-    makeCoffee(shots: number): CoffeeCup {
-      const coffee = super.makeCoffee(shots);
+    makeMilk(cup: CoffeeCup): CoffeeCup {
+      this.steamMilk();
       return {
-        ...coffee,
-        hasSugar: true,
+        ...cup,
+        hasMilk: true,
       };
     }
   }
 
-  const machines: CoffeeMaker[] = [
-    //이렇게 implement된 interface를 사용하면 해당 interface에 있는 기능만 호출이 가능하다.
-    new CoffeeMachine(16),
-    new CaffeLatteMachine(16, "asd"),
-    new SweetCoffeMaker(16),
-    new CoffeeMachine(16),
-    new CaffeLatteMachine(16, "asd"),
-    new SweetCoffeMaker(16),
-  ];
+  //설탕 제조기
+  class AutomaticSugarMixer {
+    private getSugar(): boolean {
+      console.log("gettinh some sugar from candy");
+      return true;
+    }
 
-  /*
-  const machines: = [       이렇게 일반 적인 형태로 사용하면 요 아이들이 가지고 있는 공통 요소들을 사용할 수 있다.
-    new CoffeeMachine(16),
-    new CaffeLatteMachine(16, "asd"),
-    new SweetCoffeMaker(16),
-    new CoffeeMachine(16),
-    new CaffeLatteMachine(16, "asd"),
-    new SweetCoffeMaker(16),
-  ];
-  */
-  machines.forEach((machine) => {
-    console.log("==============================================");
-    machine.makeCoffee(1); //이게 바로 엄청난 장점이다. 어떤 클래스이던지 상관없이 하나의 메소드로 모두 동작을 수행하고 있다.
-  });
+    addSugar(cup: CoffeeCup): CoffeeCup {
+      const sugar = this.getSugar();
+      return {
+        ...cup,
+        hasSugar: sugar,
+      };
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(
+      beans: number,
+      public readonly serialNumber: string,
+      private milkFother: CheapMilkSteamer //이런식으로 필요한 모듈을 가져와 사용할 수 있다. composition이다.
+    ) {
+      super(beans);
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.milkFother.makeMilk(coffee); // 가져온 모듈을 통해서 이렇게 사용이 가능하다.
+    }
+  } // 기본적으로 extends를 사용해서 상속이 가능하다.
+
+  class SweetCoffeMaker extends CoffeeMachine {
+    constructor(private beans: number, private sugar: AutomaticSugarMixer) {
+      super(beans);
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.sugar.addSugar(coffee); // 가져온 모듈을 통해서 기존의 작업을 해준다.
+    }
+
+    getSugar() {
+      console.log("getting some sugar");
+    }
+  }
+
+  class SweetCaffeLatteMachine extends CoffeeMachine {
+    // composition 을 이용해서 작업을 수행한다. 모듈을 이것 저것 가져와서 작업하는 거랑 비슷하다.
+    constructor(
+      private beans: number,
+      private milk: CheapMilkSteamer,
+      private sugar: AutomaticSugarMixer
+    ) {
+      super(beans);
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return this.milk.makeMilk(this.sugar.addSugar(coffee));
+    }
+  }
+  // 위의 클래스는 좋아 보이지만 결정적인 단점이 있다. 그것은 클래스간의 결합력이 크다는 것이다. 클래스간의 결합력이 낮을 수록 좋다 => 각각의 객체는 서로의 작업에 있어 독립성을 유지해야 한다.
 }
