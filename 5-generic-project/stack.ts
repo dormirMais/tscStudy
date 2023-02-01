d04853e80d37b1dde07d95d6cabcc4d1f87e5b1f
dormirMais <soonhak1212@naver.com>
2023-02-01 22:06:13 +0900

    제네릭을 이용한 스택구현

--- 5-generic-project/stack.ts
+++ 5-generic-project/stack.ts
@@ -1,30 +1,30 @@
-interface Stack {
+interface Stack<T> {
   readonly size: number;
-  push(value: string): void;
-  pop(): string;
+  push(value: T): void;
+  pop(): T;
 }
 
-type StackNode = {
-  value: string;
-  next?: StackNode;
+type StackNode<T> = {
+  value: T;
+  next?: StackNode<T>;
 };
 
-class StackImpl implements Stack {
+class StackImpl<T> implements Stack<T> {
   private _size: number = 0;
-  private head?: StackNode;
+  private head?: StackNode<T>;
 
   constructor(private capacity: number) {}
   get size() {
     //interface 에서 readonly로 지정을 했기 때문에 getter로 만듬.
     return this._size;
   }
-  push(value: string) {
+  push(value: T) {
     if (this.size === this.capacity) throw new Error("stack is full");
-    const node: StackNode = { value, next: this.head };
+    const node: StackNode<T> = { value, next: this.head };
     this.head = node;
     this._size++;
   }
-  pop(): string {
+  pop(): T {
     if (this.head == null) {
       throw new Error("stack is empty");
     }
@@ -35,10 +35,17 @@ class StackImpl implements Stack {
   }
 }
 
-const stack = new StackImpl(100);
+const stack = new StackImpl<string>(100);
 stack.push("haki");
 stack.push("poki");
 stack.push("ponki");
 while (stack.size !== 0) {
   console.log(stack.pop());
+}
+const stack2 = new StackImpl<number>(100);
+stack2.push(123);
+stack2.push(234);
+stack2.push(345);
+while (stack.size !== 0) {
+  console.log(stack2.pop());
 }
