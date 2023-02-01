18c8b61c6177ef2c4cf928c2918a60f4c17fe4f5
dormirMais <soonhak1212@naver.com>
2023-02-01 17:56:35 +0900

    generic constrains

--- 5-generic/5-3-constrains.ts
+++ 5-generic/5-3-constrains.ts
@@ -39,3 +39,25 @@ function pay<T extends Employee>(employee: T): T {
   employee.pay();
   return employee;
 }
+
+const obj = {
+  name: "haki",
+  age: 100,
+};
+
+const obj2 = {
+  animal: "🐈",
+};
+
+// 이런식으로 좀 복잡하게 구성이 된다. T와 K를 이용하는데 문제는 K가 T의 key인지 그냥 적어서는 알 수 없다는 것이다.
+// 이를 해결하기 위해서 extends key of를 이용한다.
+// 문맥적으로 보았을 때 K가 T의 키라는 의미는 T가 obj의 형태로 구성되어 있다는 것을 내포한다.
+
+function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
+  return obj[key];
+}
+
+// 제네릭으로 K값에 대한 제한을 주었기 때문에 2번째 param에 잘못된 K값을 넣어주게 되면 TSC가 자동으로 에러를 알려준다.
+console.log(getValue(obj, "name")); // haki
+console.log(getValue(obj, "age")); // 100
+console.log(getValue(obj2, "animal")); // 🐈
